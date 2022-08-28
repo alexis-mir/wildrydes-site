a2771cfc067bb403a9ee314729f814e6ee06f7fa
alexis-mir <alexis_mir@hotmail.com>
2022-08-28 01:30:55 -0300

    add cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_OkNLgXSO8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4h5mplm1vf27fqlmm92iln46vo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
